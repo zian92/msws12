d46c5266a439f211b8353d9be159a9c7d2e41c12
Jonas <xyxyxyz@hotmail.de>
2012-12-19 12:04:08 +0100

    bilder upload

---
+++ 38/ImageScaler.java
@@ -0,0 +1,80 @@
+/**
+ *  Uebungsgruppe _____
+ *  (Namen der Teilnehmer)
+ *
+ */
+
+public class ImageScaler {
+
+    private Picture input;
+
+    public ImageScaler() {
+
+	//  Erstelle Dialog zur Auswahl einer Bilddatei und lade das
+	//  ausgewaehlte Bild.
+	input = new Picture(FileChooser.pickAFile());
+
+    }
+
+    /**
+     * Skaliere ein Bild.
+     * @param xScale Skalierungsfaktor in x-Richtung
+     * @param yScale Skalierungsfaktor in y-Richtung
+     * @return Picture Referenz auf das skalierte Bild
+     */
+    public Picture scale(double xScale, double yScale) {
+	
+	//  Referenzvariable, die auf das zu erstellende Bild verweisen soll.
+	Picture output = null;
+	
+	//  Falls keine Datei ausgewaehlt wurde, ist nichts zu tun.
+	if (input != null) {
+
+	    //  Zeige das Bild zur Kontrolle an.
+	    input.show();	    
+	    
+	    /*
+	      Hier ist die Loesung zu Aufgabe 38 einzutragen.
+	    */
+
+	    //  Zeige das Bild zur Kontrolle an.
+	    output.show();	    
+	
+	}
+
+	return output;
+    }
+
+    /**
+     * Loesung der Aufgabe 38 / Blatt 10 / Wintersemester 2012/2013.
+     */
+    public void run() {
+
+	Picture output;
+
+	String directory = "";   //  Hier muss ein gueltiger Verzeichnisname 
+	                         //  eingesetzt werden,
+                               	 //  z.B. "C:\" oder "/Users/Shared/" oder ...
+	
+	//  Erster Test
+	output = scale( 2.00, 1.00);
+	output.write(directory + "scaled0.png");
+
+	//  Zweiter Test
+	output = scale( 1.00, 1.50);
+	output.write(directory + "scaled1.png");
+
+	//  Dritter Test
+	output = scale( 0.36, 2.79);
+	output.write(directory + "scaled2.png");
+
+    }
+
+    public static void main(String[] args) {
+
+	ImageScaler is = new ImageScaler();	
+	is.run();
+
+    }
+
+}
---
+++ 40/Loesung40.java
@@ -0,0 +1,48 @@
+/**
+ * Uebungsgruppe G6E (Namen der Teilnehmer)
+ * 
+ */
+
+public class Loesung40 {
+
+	/**
+	 * Wandele eine Zahl zwischen 1 und 3000 (jeweils inklusive) in ihre Darstellung im roemischen Zahlsystem um.
+	 * 
+	 * @param n
+	 *            umzuwandelnde Zahl
+	 * @return String Referenz auf die Repraesentation der umgewandelten Zahl.
+	 */
+	public String convertNumber(int n) {
+		return null;
+
+		/*
+		 * Hier ist die Loesung zu Aufgabe 38 einzutragen.
+		 */
+
+	}
+
+	/**
+	 * Loesung der Aufgabe 40 / Blatt 10 / Wintersemester 2012/2013.
+	 */
+	public void run() {
+
+		// Beispieltests.
+		System.out.println("   1 = " + convertNumber(1)); // Sollte "I" ergeben.
+		System.out.println("   4 = " + convertNumber(4)); // Sollte "IV" ergeben.
+		System.out.println("   9 = " + convertNumber(9)); // Sollte "IX" ergeben.
+		System.out.println("  17 = " + convertNumber(17)); // Sollte "XVII" ergeben.
+		System.out.println("  47 = " + convertNumber(47)); // Sollte "XLVII" ergeben.
+		System.out.println("  99 = " + convertNumber(99)); // Sollte "XCIX" ergeben.
+		System.out.println("1000 = " + convertNumber(1000)); // Sollte "M" ergeben.
+		System.out.println("1498 = " + convertNumber(1498)); // Sollte "MCDXCVIII" ergeben.
+		System.out.println("2999 = " + convertNumber(2999)); // Sollte "MMCMXCIX" ergeben.
+
+	}
+
+	public static void main(String[] args) {
+
+		Loesung40 meineLoesung = new Loesung40();
+		meineLoesung.run();
+
+	}
+}
