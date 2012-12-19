b0bb4e959b56838e24c5dac57d6cf73f076ef36a
Jonas <xyxyxyz@hotmail.de>
2012-12-19 12:28:41 +0100

    gerüst fertig

--- 40/Loesung40.java
+++ 40/Loesung40.java
@@ -11,6 +11,7 @@
 public class Loesung40 {
 
 	private final String ZTAG = "test";
+
 	/**
 	 * Wandele eine Zahl zwischen 1 und 3000 (jeweils inklusive) in ihre Darstellung im roemischen Zahlsystem um.
 	 * 
@@ -20,13 +21,49 @@ public class Loesung40 {
 	 */
 	public String convertNumber(int n) {
 		String number = String.valueOf(n);
-		String romanNumber ="";
-		
-		if(number.length() > 4){
+		String romanNumber = "";
+		if (number.length() > 4) {
 			number = number.substring(0, 4);
 		}
-		return number;
-	//	return romanNumber;
+		if(Integer.valueOf(number) > 1000) {
+			romanNumber = this.convertTausender(number);
+			romanNumber = romanNumber + this.convertHunderter(number);
+			romanNumber = romanNumber + this.convertZehner(number);
+			romanNumber = romanNumber + this.convertEiner(number);
+		}
+		number = this.convertEiner(number);
+		
+		
+		 return romanNumber;
+	}
+	
+	private String convertZehner(String temp){
+		
+		return temp;
+	}
+
+	private String convertHunderter(String temp) {
+		return temp;
+	}
+	
+	private String convertTausender(String temp){
+		return temp;
+	}
+	
+	private String convertEiner(String temp) {
+		switch (Integer.valueOf(temp)) {
+			case 1: temp = "I"; break;
+			case 2:temp = "II"; break;
+			case 3:temp = "III"; break;
+			case 4:temp = "IV"; break;
+			case 5:temp = "V"; break;
+			case 6:temp = "VI"; break;
+			case 7:temp = "VII"; break;
+			case 8:temp = "VIII"; break;
+			case 9:temp = "IX"; break;
+			default:
+		}
+		return temp;
 	}
 
 	/**
@@ -44,8 +81,6 @@ public class Loesung40 {
 		System.out.println("1000 = " + convertNumber(1000)); // Sollte "M" ergeben.
 		System.out.println("1498 = " + convertNumber(1498)); // Sollte "MCDXCVIII" ergeben.
 		System.out.println("2999 = " + convertNumber(2999)); // Sollte "MMCMXCIX" ergeben.
-		//System.out.println("2999 = " + convertNumber(2900909)); // Sollte "MMCMXCIX" ergeben.
-
 	}
 
 	public static void main(String[] args) {
