79d073033ec9807b2c0992419891e36655ff359f
Jonas <xyxyxyz@hotmail.de>
2012-12-30 19:25:44 +0100

    added classes 2nd folder

---
+++ 39/src/Loesung39.java
@@ -0,0 +1,25 @@
+/**
+ * Uebungsgruppe G6E (Namen der Teilnehmer)
+ * 
+ */
+
+/**
+ * 
+ * @author Jonas Stadler
+ * @version 1.0
+ */
+public class Loesung39 {
+
+	public Loesung39() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
