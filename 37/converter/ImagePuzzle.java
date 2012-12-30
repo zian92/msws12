dcddea8002bebdcf396f202e35f0fbe51e673037
Jonas <xyxyxyz@hotmail.de>
2012-12-30 20:24:58 +0100

    37a) one last error to fix -> save the new file

--- 37/converter/ImagePuzzle.java
+++ 37/converter/ImagePuzzle.java
@@ -25,29 +25,29 @@ public class ImagePuzzle {
 			try {
 				pix = input.getPixels();
 				System.out.println("pixel bekommen");
-				
-			for (Pixel p : pix) {
-				String s = "X: " + p.getX() + " Y: " + p.getY();
-				System.out.println("start " + s);
-				p.setBlue(p.getBlue() * 20);
-				p.setGreen(p.getGreen() * 20);
-				p.setRed(0);
-				System.out.println("end   " + s);
-			}
-			output.setPictureFrame(pix);
+output = input;
+				for (Pixel p : pix) {
+					String s = "X: " + p.getX() + " Y: " + p.getY();
+					System.out.println("start " + s);
+					int b = (p.getBlue() * 20);
+					int g = (p.getGreen() * 20);
+					int r = 0;
+					output.setBasicPixel(p.getX(), p.getY(), r*65536+g*256+b); // set a pixel at described position, with rgb value
+					System.out.println("end   " + s);
+				}
 				// Zeige das auszugebende Bild an.
 				output.show();
 
-			// Schreibe das auszugebende Bild in eine Datei.
-			String directory = "./"; // Hier muss ein gueltiger Verzeichnisname
-			// eingesetzt werden,
-			// z.B. "C:\" oder "/Users/Shared/" oder ...
-			output.write(directory + "black-puzzle-solution.png");
+				// Schreibe das auszugebende Bild in eine Datei.
+				String directory = "./"; // Hier muss ein gueltiger Verzeichnisname
+				// eingesetzt werden,
+				// z.B. "C:\" oder "/Users/Shared/" oder ...
+				output.write(directory + "black-puzzle-solution.png");
 			} catch (Exception e) {
 				System.out.println("error!");
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
 
@@ -77,7 +77,7 @@ public class ImagePuzzle {
 			output.show();
 
 			// Schreibe das auszugebende Bild in eine Datei.
-			String directory = "";
+			String directory = FileChooser.getMediaDirectory();
 			// Hier muss ein gueltiger Verzeichnisname
 			// eingesetzt werden,
 			// z.B. "C:\" oder "/Users/Shared/" oder ...
@@ -90,8 +90,8 @@ public class ImagePuzzle {
 		ImagePuzzle ip = new ImagePuzzle();
 
 		ip.solveBlackPuzzle(); // Loese Aufgabe 37(a).
-		//ip.solveGrassPuzzle(); // Loese Aufgabe 37(b).
-
+		// ip.solveGrassPuzzle(); // Loese Aufgabe 37(b).
+System.exit(0);
 	}
 
 }
