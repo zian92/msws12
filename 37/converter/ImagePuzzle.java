aa90fd01ff695f80af936f3dff0bbd26e9c6b9cc
zian92 <xyxyxyz@hotmail.de>
2013-01-01 20:39:41 +0100

    37b) fertig, irgendein fehler...

--- 37/converter/ImagePuzzle.java
+++ 37/converter/ImagePuzzle.java
@@ -34,13 +34,12 @@ public class ImagePuzzle {
 				int g = (p.getGreen() * 20);
 				int r = 0;
 				output.setBasicPixel(p.getX(), p.getY(), r * 65536 + g * 256 + b); // set a pixel at described position, with rgb value
-				//System.out.println("end   " + s);
+				// System.out.println("end   " + s);
 			}
 			// Zeige das auszugebende Bild an.
 			input.hide();
 			output.show();
 
-			
 			try {
 				output.writeOrFail(directory + "black-puzzle-solution.png");
 			} catch (IOException e) {
@@ -64,22 +63,39 @@ public class ImagePuzzle {
 
 		// Falls keine Datei ausgewaehlt wurde, ist nichts zu tun.
 		if (input != null) {
-
+			Pixel[] pix = null;
 			// Zeige das Bild zur Kontrolle an.
 			input.show();
-
-			/*
-			 * Hier ist die Loesung zu Aufgabe 37(b) einzutragen.
-			 */
-
+			pix = input.getPixels();
+			System.out.println("pixel bekommen");
+			output = input;
+			for (Pixel p : pix) {
+				String s = "X: " + p.getX() + " Y: " + p.getY();
+				System.out.println("start " + s);
+				int b = p.getBlue();
+				if (b < 16) {
+					b = b * 16;
+				} else {
+					b = 0;
+				}
+				int g = 0;
+				int r = b;
+				output.setBasicPixel(p.getX(), p.getY(), r * 65536 + g * 256 + b); // set a pixel at described position, with rgb value
+				// System.out.println("end   " + s);
+			}
 			// Zeige das auszugebende Bild an.
+			input.hide();
 			output.show();
 
 			// Schreibe das auszugebende Bild in eine Datei.
 			// Hier muss ein gueltiger Verzeichnisname
 			// eingesetzt werden,
 			// z.B. "C:\" oder "/Users/Shared/" oder ...
-			output.write(directory + "grass-puzzle-solution.png");
+			try {
+				output.writeOrFail(directory + "grass-puzzle-solution.png");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -91,9 +107,9 @@ public class ImagePuzzle {
 		directory = directory + "\\";
 		// eingesetzt werden,
 		// z.B. "C:\" oder "/Users/Shared/" oder ...
-		ip.solveBlackPuzzle(directory); // Loese Aufgabe 37(a).
-		// ip.solveGrassPuzzle(directory); // Loese Aufgabe 37(b).
-		System.exit(0);
+		// ip.solveBlackPuzzle(directory); // Loese Aufgabe 37(a).
+		ip.solveGrassPuzzle(directory); // Loese Aufgabe 37(b).
+		//System.exit(0);
 	}
 
 }
