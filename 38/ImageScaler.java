5f894c63740694f90e65fea074848d087539f365
zian92 <xyxyxyz@hotmail.de>
2013-01-05 13:53:11 +0100

    neues gerüst -> nur noch vorberechnungen

--- 38/ImageScaler.java
+++ 38/ImageScaler.java
@@ -37,50 +37,68 @@ public class ImageScaler {
 
 		// Falls keine Datei ausgewaehlt wurde, ist nichts zu tun.
 		if (input != null) {
-
 			// Zeige das Bild zur Kontrolle an.
 			input.show();
 
 			// TODO faktoren neu berechnen!!!!!!!!!!!!!!
 			// TODO
 			// TODO
+			int newScaleX = 0;
+			int newScaleY = 0;
 			int newSX = (int) java.lang.Math.floor(input.getWidth() * (xScale));
 			int newSY = (int) java.lang.Math.floor(input.getHeight() * (yScale));
 
-			output = new Picture(newSX,newSY);
+			if (newScaleX == newScaleY && newScaleX == 1) return input; // falls keine saklierung vorgenomen wird
+			output = new Picture(newSX, newSY);
 			// int x = (int) java.lang.Math.floor(input.getWidth() / newx); // x pixel scale ->
 			// int y = (int) java.lang.Math.floor(input.getHeight() / newy); // y pixel scale
 
+			int sdUPixelX = 0;
+			int sdCPixelX = 0;
+			int sdAPixelx = 0;
+			int sdUPixelY = 0;
+			int sdCPixelY = 0;
+			int sdAPixelY = 0;
+			
+			if (newScaleX < 1) {
+				sdUPixelX = 0;
+				sdCPixelX = 0;
+			}
+			if (newScaleY < 1) {
+				sdUPixelY = 0;
+				sdCPixelY = 0;
+			}
+
 			for (int i = 0; i < input.getWidth(); i++) { // breite
 				for (int j = 0; j < input.getHeight(); j++) { // höhe
+
 					Pixel p = input.getPixel(i, j);
 					if (newSX < 1) { // verkleinerung breite
-
+						if (sdCPixelX < sdUPixelX) {
+							sdCPixelX++;
+						} else {
+							output.setBasicPixel(sdAPixelx, sdAPixelY, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
+							sdCPixelX = 0;
+						}
 					} else { // vergrößerung breite
 						for (int k = 0; k < newSX; k++) {
-							output.setBasicPixel(i+k, j, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
+							output.setBasicPixel(i + k, j, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
 						}
 					}
-
 					if (newSY < 1) {// verkleinerun höhe
-
+						if (sdCPixelY < sdUPixelY) {
+							sdCPixelY++;
+						} else {
+							output.setBasicPixel(sdAPixelx, sdAPixelY, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
+							sdCPixelY = 0;
+						}
 					} else {// vergrößerung höhe
 						for (int k = 0; k < newSY; k++) {
-							output.setBasicPixel(i, j+k, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
+							output.setBasicPixel(i, j + k, this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
 						}
 					}
 				}
 			}
-			// for (int i = 0; i < input.getWidth(); i++) {
-			// for (int j = 0; j < input.getHeight(); j++) {
-			// Pixel p = input.getPixel(i, j);
-			// System.out.println("Pixel X: " + i + " Y: " + j);
-			// for (int k = 0; k < y; k++) {// y streckung
-			// output = new Picture(i, j + k);
-			// output.setBasicPixel(p.getX(), p.getY(), this.makeRGB(p.getRed(), p.getGreen(), p.getBlue()));
-			// }
-			// }
-			// }
 
 			// Zeige das Bild zur Kontrolle an.
 			input.hide();
