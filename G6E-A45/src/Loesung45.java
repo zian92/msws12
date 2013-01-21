bfc2f863fadfc6479f4a23593b251c0f10686c56
Henner25 <kein.regen@wolke7.net>
2013-01-21 09:58:03 +0100

    vergessen :D

--- G6E-A45/src/Loesung45.java
+++ G6E-A45/src/Loesung45.java
@@ -8,8 +8,29 @@
  * @author Henner Niehaus
  * @version 1.0
  */
+import de.henner.Zooverwaltungssystem.*;
+import de.henner.Zooverwaltungssystem.Tiere.Animal;
+import de.henner.Zooverwaltungssystem.Tiere.Animal.Food;
+import de.henner.Zooverwaltungssystem.Tiere.Animal.Race;
+import de.henner.Zooverwaltungssystem.Tiere.Animal.Sex;
 public class Loesung45 {
-	public static void Main(String args[]){
+	
+	public void solve(){
 		
+		ZooMonitor moni = new ZooMonitor();
+		moni.makeNewAnimal("Cheeta", Race.CHIMP, Sex.MALE, Food.BANANA, 1645);
+		moni.makeNewAnimal("Jonas", Race.SLOTH, Sex.MALE, Food.COOKIES, 1200);
+		moni.makeNewAnimal("Eva", Race.GUINEA_PIG, Sex.FEMALE, Food.KRILL, 730);
+		for(int i = 0; i < 3; i++){
+			System.out.print(moni.getMonitoredZoo().getAnimals().get(i).getName() + "\t");
+			System.out.print(moni.getMonitoredZoo().getAnimals().get(i).getRace().toString() + "\t");
+			System.out.print(moni.getMonitoredZoo().getAnimals().get(i).getSex().toString() + "\t");
+			System.out.print(moni.getMonitoredZoo().getAnimals().get(i).getFood().toString() + "\t");
+			System.out.println(moni.getMonitoredZoo().getAnimals().get(i).getFeedingTime() + "\n");
+		}
+	}
+	
+	public static void main(String args[]){
+		new Loesung45().solve();
 	}
 }
